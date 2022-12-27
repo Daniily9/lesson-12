eeed90c2fd37a9445bd8fcfe7d002904000020ee
Daniily9 <112887456+Daniily9@users.noreply.github.com>
2022-12-27 17:24:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let circle = document.createElement("div");
+circle.style.backgroundColor = "green";
+circle.style.width = "100px";
+circle.style.height = "100px";
+circle.style.borderRadius = "50%";
+circle.style.marginLeft = "100px";
+document.body.appendChild(circle);
